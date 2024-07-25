7b56324fd75384f870e34a8cbd8b18d4e82606b5
Rupali Sawant <101975292+rupasawant95@users.noreply.github.com>
2024-07-25 10:36:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "google_storage_bucket" "my-bucket" {
   name                     = "test-githubdemo-bucket"
-  project                  = "hashicorp-317612"
-  location                 = "us-central1-a"
+  project                  = "tft-pro"
+  location                 = "us"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
 
 resource "google_storage_bucket" "my-bucket2" {
-  name                     = "tt-githubdemo-bucket-002"
-  project                  = "tt-dev-001"
-  location                 = "US"
+  name                     = "test-githubdemo-bucket-2"
+  project                  = "tft-pro"
+  location                 = "us"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
